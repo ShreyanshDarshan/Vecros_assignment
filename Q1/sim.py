5f37da694f7dc58838af31097ab93f24e9c9a9bc
Shreyansh Darshan <shreyanshdarshan1@gmail.com>
2021-04-07 08:24:49 +0530

    added comments

--- Q1/sim.py
+++ Q1/sim.py
@@ -5,40 +5,57 @@ import planner
 from multiprocessing import Process, Manager, Value, Array, Lock
 import math
 
-
+# sampling time
 DT = 0.01
+# start point of bot in x, y, z
 start_point = [1, 3, 1]
+# destination point of the bot in x, y, z
 destination_point = [20, 20, 10]
+# number of random obstacles to spawn in the world
 num_obstacles = 200
+# bot dimensions in depth (x), width (y), height (z)
 bot_size = [0.5, 0.6, 0.2]
+# how far bot can sense obstacles
 sensor_range = 10
+
+# voxel size computed based on world size
 voxel_size = math.pow(destination_point[0]*destination_point[1]*destination_point[2] / 4000, 0.3333)
 
+# class to store bot's properties
 class Bot:
+    # intialise bot's properties
     def __init__(self, start_position, geometry, range):
         self.position = np.array(start_position, dtype=float)
         self.velocity = np.array([0, 0, 0], dtype=float)
         self.mesh_box = o3d.geometry.TriangleMesh.create_box(geometry[1], geometry[0], geometry[2])
         self.sensor_range = range
 
+    # get bot's position
     def get_state(self):
         return self.position
     
+    # give bot control velocity
     def set_control(self, v):
         self.velocity = v
 
+    # simulate bot dynamics
     def update_state(self, dt):
         self.position += self.velocity*dt
 
+    # get bot's dimension
     def get_geometry(self):
         self.mesh_box.compute_vertex_normals()
         self.mesh_box.paint_uniform_color([0.9, 0.1, 0.1])
         self.mesh_box = self.mesh_box.translate((self.position[0], self.position[1], self.position[2]), relative=False)
         return self.mesh_box
 
+# class to store world's properties
 class World:
+    # initiliase world properties
     def __init__(self, destination, num, dt):
+        # list of obstacles in world
         self.obstacles = []
+        # destination = opposite extreme of world
         self.destination = np.array(destination, dtype=float)
         self.dt = dt
         self.time = 0
@@ -48,15 +65,17 @@ class World:
         self.unseen_cloud = o3d.geometry.PointCloud()
         self.generate_random_obstacles(num)
 
-
+    # add new obstacles to world
     def add_obstacle(self, obstacle):
         self.obstacles.append(np.array(obstacle, dtype = float))
         self.unseen_obs.append(len(self.obstacles)-1)
 
+    # randomly generate obstacles
     def generate_random_obstacles(self, num):
         for i in range(num):
             self.add_obstacle(np.random.rand(3) * self.destination)
 
+    # get obstacle in cloud format
     def get_obstacle_cloud(self):
         if len(self.unseen_obs)>0:
             self.unseen_cloud.points = o3d.utility.Vector3dVector(np.array([self.obstacles[i] for i in self.unseen_obs]))
@@ -66,6 +85,7 @@ class World:
             self.seen_cloud.paint_uniform_color([1, 0, 0])
         return self.seen_cloud, self.unseen_cloud
 
+    # get the obstacles visible to the bot in shared memory
     def get_nearby_obstacles(self, bot, obs_list):
         obs_list[:] = []
         o_list = []
@@ -80,15 +100,19 @@ class World:
         obs_list[:] = o_list
 
 
+    # get world sampling time
     def get_sampling_time(self):
         return self.dt
 
+    # get world time
     def get_time(self):
         return self.time
 
+    # update world time
     def update_time(self):
         self.time+=self.dt
 
+    # world bounding box in lineset form for visualisation
     def get_bounding_box(self):
         points = [
             [0, 0, 0],
@@ -121,6 +145,7 @@ class World:
         line_set.colors = o3d.utility.Vector3dVector(colors)
         return line_set
 
+# make path lineset for visualisation
 def make_path_lineset (lineset, path):
     path_list = np.reshape(np.array(path), (int(len(path)/3), 3)).tolist()
     lines = []
@@ -130,15 +155,16 @@ def make_path_lineset (lineset, path):
     lineset.points = o3d.utility.Vector3dVector(path_list)
     lineset.lines = o3d.utility.Vector2iVector(lines)
     lineset.colors = o3d.utility.Vector3dVector(colors)
-    # print (path_list)
     return lineset
 
+# function to normalise a vector
 def normalize(v):
     norm = np.linalg.norm(v)
     if norm == 0: 
        return v
     return v / norm
 
+# calculate and give control to bot based on current path
 def give_control_input(vel, state, path):
     path_list = np.reshape(np.array(path), (int(len(path)/3), 3)).tolist()
     if len(path_list) > 0:
@@ -146,16 +172,16 @@ def give_control_input(vel, state, path):
         vel[:] = (normalize(dir_vector)*2).tolist()
         if np.linalg.norm(np.array(state)-np.array(path_list[0])) < 0.5:
             path_list.pop(0)
-            path[:] = np.ndarray.flatten(np.array(path_list)).tolist()  
-        # print("non zeros", vel[:])
+            path[:] = np.ndarray.flatten(np.array(path_list)).tolist()
     else:
         vel[:] = np.zeros(3, dtype=float).tolist()
-        # print("zeros", vel[:])
 
 if __name__ == '__main__':
+    # initialise world and bot
     world = World(destination_point, num_obstacles, DT)
     bot = Bot(start_point, bot_size, sensor_range)
 
+    # setup visualiser
     path_line = o3d.geometry.LineSet()
     axes = o3d.geometry.TriangleMesh.create_coordinate_frame()
     vis = o3d.visualization.Visualizer()
@@ -172,6 +198,7 @@ if __name__ == '__main__':
     ctr.set_up(up=[-1, -1, -1])
     ctr.set_front(front=[1, 1, 1])
 
+    # setup shared variables
     manager = Manager()
     lock = Lock()
     bot_control = Array('d', [0, 0, 0])
@@ -182,31 +209,39 @@ if __name__ == '__main__':
     bot_size_shared = Array('d', bot_size)
     world_bounds = Array('d', destination_point)
 
+    # start astar path planning parallel process
     plan = Process(target=planner.get_path, args=(lock, bot_state, bot_size_shared, world_bounds, voxel_size_shared, obstacles_in_view, path))
     plan.start()
 
-    time = 0
-
+    # simulation loop
     while True:
+        # get obstacles in bot's view
         world.get_nearby_obstacles(bot, obstacles_in_view)
-        bot_list = bot.get_state().tolist()
 
+        # calculate control input from path
         lock.acquire()
         give_control_input(bot_control, bot_state, path)
         lock.release()
 
+        # give control input to bot
         bot.set_control(np.array(bot_control))
+        # run bot dynamics
         bot.update_state(world.get_sampling_time())
+        # update bot state shared variable
         bot_state[:] = bot.get_state().tolist()
+        # update world time
         world.update_time()
 
         lock.acquire()
 
+        # show world time and control input
         print ("time:", world.get_time(), "     control_input:", bot_control[:])
 
+        # path visualisation
         make_path_lineset(path_line, path)
         lock.release()
 
+        # update visualiser
         vis.update_geometry(path_line)
         seen, unseen = world.get_obstacle_cloud()
         vis.update_geometry(seen)
--- Q2/segment_plane.py
+++ Q2/segment_plane.py
@@ -2,16 +2,21 @@ import numpy as np
 import open3d as o3d
 import random
 
+# voxel size for voxel filter applied. set this to the maximum value that doesnt remove too much details (which will depend on the scale of the scene)
 VOXEL_SIZE = 0.1
 DIST_THRESH = 2*VOXEL_SIZE
+
+# number of iterations for random sampling
 ITERATIONS = 200
 
+# function to normalise a vector
 def normalize(v):
     norm = np.linalg.norm(v)
     if norm == 0: 
        return v
     return v / norm
 
+# sample a random plane from 3 points
 def sample_plane(cloud):
     p1 = random.randint(0, cloud.shape[0])
     p2 = random.randint(0, cloud.shape[0])
@@ -20,6 +25,7 @@ def sample_plane(cloud):
     vec2 = cloud[p3] - cloud[p1]
     return cloud[p1], normalize(np.cross(vec1, vec2))
 
+# get the number of point lying in the plane
 def consensus(cloud, pt, normal):
     diff = cloud - pt
     dot = np.matmul(diff, np.vstack(normal))
@@ -28,6 +34,7 @@ def consensus(cloud, pt, normal):
     sum = inliers.shape[0]
     return sum
 
+# get the inlier cloud of the plane
 def get_inliers(cloud, pt, normal):
     inliers = []
     diff = cloud - pt
@@ -38,9 +45,11 @@ def get_inliers(cloud, pt, normal):
     outliers = cloud[dist[0] >= DIST_THRESH]
     return inliers, outliers
 
+# key for sorting the consensus list
 def get_key(e):
     return e[2]
 
+# get axis with z' normal to given plane
 def get_axis(p, normal):
     i_vec_1 = normalize(np.cross(normal, np.array([1, 0, 0])))
     i_vec_2 = normalize(np.cross(normal, np.array([0, 1, 0])))
@@ -55,46 +64,53 @@ def get_axis(p, normal):
         j_vec = np.cross(normal, i_vec_3)
         return np.array([i_vec_3, j_vec, normal])
 
+# transform the pointcloud so that the plane lies flat on the ground
 def transform_cloud(cloud, axis):
     rot_mat = np.transpose(axis)
     rot_cloud = np.matmul(cloud, rot_mat)
     return rot_cloud
 
+# get surface area by looking at the plane from top and discretising to calculate surface area
 def get_surface_area(cloud, p, normal):
     selected_cloud, _ = get_inliers(cloud, p, normal)
     rot_cloud = transform_cloud(selected_cloud, get_axis(p, normal))
     flat_cloud = o3d.geometry.PointCloud()
     flat_cloud.points = o3d.utility.Vector3dVector(np.matmul(rot_cloud, np.array([[1, 0, 0], [0, 1, 0], [0, 0, 0]])))
     flat_cloud_down = flat_cloud.voxel_down_sample(VOXEL_SIZE)
-    # o3d.visualization.draw_geometries([flat_cloud_down])
     num_points = np.asarray(flat_cloud_down.points).shape[0]
     return VOXEL_SIZE*VOXEL_SIZE*num_points
 
 print("program_started")
+# load boxes.pcd, change this string to change input file 
 pcd = o3d.io.read_point_cloud("./boxes.pcd")
+# downsample the pointcloud to make uniform density cloud
 downpcd = pcd.voxel_down_sample(VOXEL_SIZE)
+# convert to numpy array
 cloud = np.asarray(downpcd.points)
-# o3d.visualization.draw_geometries([downpcd])
 
+# perform RANSAC plane fitting and make a consensus list
 consensus_list = []
 for i in range(ITERATIONS):
     p, normal = sample_plane(cloud)
     if np.linalg.norm(normal) != 0:
         c = consensus(cloud, p, normal)
-        # print(i, c)
         consensus_list.append([p, normal, c])
 
+# sort the consensus list according to the number of votes
 consensus_list.sort(reverse=True, key=get_key)
 
+# get the inlier and outlier clouds for the largest plane
 inliers, outliers = get_inliers(cloud, consensus_list[0][0], consensus_list[0][1])
 inlier_cloud = o3d.geometry.PointCloud()
 outlier_cloud = o3d.geometry.PointCloud()
 inlier_cloud.points = o3d.utility.Vector3dVector(inliers)
 outlier_cloud.points = o3d.utility.Vector3dVector(outliers)
 
+# paint the inlier cloud green and outlier cloud red
 inlier_cloud.paint_uniform_color([0, 1, 0])
 outlier_cloud.paint_uniform_color([1, 0, 0])
 
+# print the surface area of biggest plane
 print("surface area =", get_surface_area(cloud, consensus_list[0][0], consensus_list[0][1]), "m^2")
 
 o3d.visualization.draw_geometries([inlier_cloud + outlier_cloud])
